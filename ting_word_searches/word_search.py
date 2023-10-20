00475160c87fb329d36413d7e46d91c7b86a8d1d
Pedro Medeiros <pedropwks@gmail.com>
2023-10-20 08:08:52 -0300

    Feat: Adicionando funções do módulo word_search

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,54 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for i in range(len(instance)):
+        archive = instance.search(i)
+        word_true = []
+
+        for index, search_word in enumerate(archive["linhas_do_arquivo"], 1):
+            word_lower = word.lower()
+            search_word_lower = search_word.lower()
+
+            if word_lower in search_word_lower:
+                word_true.append({"linha": index})
+
+        if len(word_true) > 0:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": archive["nome_do_arquivo"],
+                    "ocorrencias": word_true,
+                }
+            )
+        else:
+            return []
+
+    return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for i in range(len(instance)):
+        archive = instance.search(i)
+        word_true = []
+
+        for index, search_word in enumerate(archive["linhas_do_arquivo"], 1):
+            word_lower = word.lower()
+            search_word_lower = search_word.lower()
+
+            if word_lower in search_word_lower:
+                word_true.append({"linha": index, "conteudo": search_word})
+
+        if len(word_true) > 0:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": archive["nome_do_arquivo"],
+                    "ocorrencias": word_true,
+                }
+            )
+        else:
+            return []
+
+    return result
