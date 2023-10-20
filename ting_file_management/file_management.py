043489bff685dc543b824e8d50802177179984ba
Pedro Medeiros <pedropwks@gmail.com>
2023-10-20 04:30:59 -0300

    Feat: Adicionando função txt_importer dentro do módulo file_management capaz de importar notícias a partir de um arquivo TXT.

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file.endswith(".txt"):
+        try:
+            with open(path_file, "r") as file:
+                return file.read().split("\n")
+        except FileNotFoundError:
+            print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+    else:
+        print("Formato inválido", file=sys.stderr)
