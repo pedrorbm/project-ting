8ae5010ffa026e2a1857704b7bca5c27cc99c1ee
Pedro Medeiros <pedropwks@gmail.com>
2023-10-20 05:47:43 -0300

    Feat: Adicionando função remove dentro do módulo file_process capaz de remover o primeiro arquivo processado

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -21,7 +21,12 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        return sys.stdout.write("Não há elementos\n")
+
+    removed = instance.dequeue()
+    nome_arquivo = removed["nome_do_arquivo"]
+    return sys.stdout.write(f"Arquivo {nome_arquivo} removido com sucesso\n")
 
 
 def file_metadata(instance, position):
